567845af5a5ad9deb819b43fbdaeaa4b25b79844
angrybirdpark <thesmileless90@gmail.com>
2022-03-24 13:39:21 +0900

    Modify : text > null : true 설정

--- postings/models.py
+++ postings/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     user       = models.ForeignKey('users.User', on_delete=models.CASCADE)
-    text       = models.TextField()
+    text       = models.TextField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
